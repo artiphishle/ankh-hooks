2e7db3445ef9e77c858096a59cae28d228a50f1a
artiphishle <fabio.gartenmann@protonmail.com>
2024-07-30 21:00:13 +0200

    Create random hues

--- lib/hooks/color/useColorPalette.ts
+++ lib/hooks/color/useColorPalette.ts
@@ -1,8 +1,11 @@
+import { EAnkhColorTone } from "ankh-types";
+
 const generateHslColor = ({ hue, range }: any) => {
   const { s, l } = range;
+  const realHue = Math.floor(Math.random() * 360);
   const saturation = Math.random() * (s.max - s.min) + s.min;
   const lightness = Math.random() * (l.max - l.min) + l.min;
-  return `hsl(${hue}, ${saturation}% ${lightness}%)`;
+  return `hsl(${realHue}, ${saturation}% ${lightness}%)`;
 }
 function generatePalette({ count = 4, hue, range }: IGeneratePalette) {
   return new Array(count).fill({ hue, range }).map(() => generateHslColor({ hue, range }))
@@ -46,21 +49,21 @@ function useEarthPalette({ count, hue }: IUseColorPalette) {
     s: { min: 36, max: 41 },
     l: { min: 36, max: 77 }
   };
-  return generatePalette({ count, hue, range, tone: EColorTone.Earth });
+  return generatePalette({ count, hue, range, tone: EAnkhColorTone.Earth });
 }
 function useFluorescentPalette({ count, hue }: IUseColorPalette) {
   const range = {
     s: { min: 63, max: 100 },
     l: { min: 82, max: 100 }
   };
-  return generatePalette({ count, hue, range, tone: EColorTone.Fluorescent });
+  return generatePalette({ count, hue, range, tone: EAnkhColorTone.Fluorescent });
 }
 function useJewelPalette({ count, hue }: IUseColorPalette) {
   const range = {
     s: { min: 73, max: 83 },
     l: { min: 56, max: 76 }
   };
-  return generatePalette({ count, hue, range, tone: EColorTone.Jewel });
+  return generatePalette({ count, hue, range, tone: EAnkhColorTone.Jewel });
 
 }
 function useNeutralPalette({ count, hue }: IUseColorPalette) {
@@ -68,21 +71,21 @@ function useNeutralPalette({ count, hue }: IUseColorPalette) {
     s: { min: 1, max: 10 },
     l: { min: 70, max: 90 }
   }
-  return generatePalette({ count, hue, range, tone: EColorTone.Neutral });
+  return generatePalette({ count, hue, range, tone: EAnkhColorTone.Neutral });
 };
 function usePastelPalette({ count, hue }: IUseColorPalette) {
   const range = {
     s: { min: 14, max: 21 },
     l: { min: 89, max: 96 }
   };
-  return generatePalette({ count, hue, range, tone: EColorTone.Pastel });
+  return generatePalette({ count, hue, range, tone: EAnkhColorTone.Pastel });
 }
 function useShadesPalette({ count, hue }: IUseColorPalette) {
   const range = {
     s: { min: 0, max: 0 },
     l: { min: 0, max: 100 }
   };
-  return generatePalette({ count, hue, range, tone: EColorTone.Shades });
+  return generatePalette({ count, hue, range, tone: EAnkhColorTone.Shades });
 };
 
 export function useColorPalette() {
@@ -97,17 +100,9 @@ export function useColorPalette() {
 }
 
 interface IGeneratePalette extends IUseColorPalette {
-  count?: number;
   range: IHslRange,
-  tone: EColorTone
-}
-enum EColorTone {
-  Earth,
-  Fluorescent,
-  Jewel,
-  Neutral,
-  Pastel,
-  Shades
+  tone: EAnkhColorTone,
+  count?: number;
 }
 interface IUseColorPalette {
   hue: number
