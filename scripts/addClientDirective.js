56b4d321ffbb815b8123c47595df9a58a962dea2
artiphishle <fabio.gartenmann@protonmail.com>
2025-04-10 18:31:48 +0200

    Fix 'use server' directive should stay if there

--- scripts/addClientDirective.js
+++ scripts/addClientDirective.js
@@ -4,9 +4,10 @@ const fs = require("fs")
 
 function addClientDirective(filePath) {
   const content = fs.readFileSync(filePath, "utf8")
+  const serverDirective = '"use server"';
   const clientDirective = '"use client";\n'
 
-  if (content.startsWith(clientDirective)) return
+  if (content.startsWith(serverDirective) || content.startsWith(clientDirective)) return
 
   fs.writeFileSync(filePath, `${clientDirective}${content}`, "utf8")
 }
